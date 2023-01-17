5eb01f49ffd8ba16d60f2d904b64f1628d8c0f24
bbookng <bbookng@gmail.com>
2023-01-17 17:09:34 +0900

    📚 ERD 완성

---
+++ 김보경/0117/funteer/src/main/java/com/yam/funteer/FunteerApplication.java
@@ -0,0 +1,13 @@
+package com.yam.funteer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunteerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunteerApplication.class, args);
+	}
+
+}
