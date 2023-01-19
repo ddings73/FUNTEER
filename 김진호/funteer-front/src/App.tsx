a88dd3ec2078fd5daaa5f5c85147b823f48a3a5c
bangbongbim <dkdpdl12311@gmail.com>
2023-01-19 17:19:08 +0900

    🔧 FrontEnd 프로젝트 생성

---
+++ 김진호/funteer-front/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+import styles from './App.module.scss';
+
+function App() {
+  return <Button variant="contained">Hello World</Button>;
+}
+
+export default App;
