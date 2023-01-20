e79dece945e8209b5a74f33f83bca49b022ee670
ddings <ddings7303@gmail.com>
2023-01-20 14:22:08 +0900

    ✨ 공통코드 entity
    Former-commit-id: 5f439616066c44c222f77a1f1c3429b74e152a83

---
+++ backend/src/main/java/com/yam/funteer/code/GroupCode.java
@@ -0,0 +1,19 @@
+package com.yam.funteer.code;
+
+public enum GroupCode {
+
+	A01("회원", "일반");
+
+
+
+
+
+
+
+
+
+
+	private String group;
+	private String description;
+	GroupCode(String group, String description) {}
+}
