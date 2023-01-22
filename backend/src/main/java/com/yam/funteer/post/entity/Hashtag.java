0b638fbb985738afae6227c21e3431a78a41c86a
rlthdqls <rlthdqls@gmail.com>
2023-01-22 22:34:24 +0900

    ✨ Post Entity 추가
    Former-commit-id: f88e811c73df438b35cef15c7a5c8e1e979daa22

---
+++ backend/src/main/java/com/yam/funteer/post/entity/Hashtag.java
@@ -0,0 +1,33 @@
+package com.yam.funteer.post.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="hashtag")
+public class Hashtag {
+	@Id
+	@Column(name="hashtag_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name="hashtag_name")
+	private String name;
+
+	@OneToMany(mappedBy = "hashtag")
+	private List<PostHashtag>postHashtagList=new ArrayList<>();
+}
