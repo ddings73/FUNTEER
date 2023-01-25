a72fe75d1c31c5ad43a6244b153406a4a13af551
ddings <ddings7303@gmail.com>
2023-01-25 17:06:56 +0900

    ✨ : user 디렉토리 하위에만 적용되게 변경
    Former-commit-id: 169fc2c402c0d6e628db1d16d7a815dfd423fb95

--- backend/src/main/java/com/yam/funteer/exception/handler/UserExceptionHandler.java
+++ backend/src/main/java/com/yam/funteer/exception/handler/UserExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.yam.funteer.exception.handler;
 
+import com.yam.funteer.common.BaseResponseBody;
 import com.yam.funteer.exception.EmailDuplicateException;
 import com.yam.funteer.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
@@ -7,16 +8,30 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice("com.yam.funteer.user")
 public class UserExceptionHandler {
 
     @ExceptionHandler(EmailDuplicateException.class)
-    public ResponseEntity<String> handleEmailDuplicateException(EmailDuplicateException ex){
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    public ResponseEntity<BaseResponseBody> handleEmailDuplicateException(EmailDuplicateException ex){
+        log.info("Exception 발생 => {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+            .body(BaseResponseBody.of("중복 이메일입니다."));
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+    public ResponseEntity<BaseResponseBody> handleUserNotFoundException(UserNotFoundException ex){
+        log.info("Exception 발생 => {}", ex.getMessage());
+        return ResponseEntity.badRequest()
+            .body(BaseResponseBody.of("존재하지 않는 회원입니다. {}"));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<BaseResponseBody> handleIllegalArgumentException(IllegalArgumentException ex){
+        log.info("Exception 발생 => {}", ex.getMessage());
+        return ResponseEntity.badRequest()
+            .body(BaseResponseBody.of("비정상적인 입력입니다."));
     }
 }
