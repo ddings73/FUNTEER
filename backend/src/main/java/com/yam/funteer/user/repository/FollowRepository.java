871df177b3fb471f3aa9a1feb14c5222fbfbcd86
ddings <ddings7303@gmail.com>
2023-01-26 11:24:27 +0900

    ✨ : 팔로우 기능 추가
    Former-commit-id: 4719592a582930236c5c70d954fa5ffae314c4be

--- backend/src/main/java/com/yam/funteer/user/repository/FollowRepository.java
+++ backend/src/main/java/com/yam/funteer/user/repository/FollowRepository.java
@@ -9,7 +9,7 @@ import com.yam.funteer.user.entity.Member;
 import com.yam.funteer.user.entity.Team;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
-	long countAllByTeamId(Long teamId);
-
-	Optional<Follow> findByMemberTeam(Member member, Team team);
+	long countAllByTeam(Team team);
+	long countAllByMember(Member member);
+	Optional<Follow> findByMemberAndTeam(Member member, Team team);
 }
