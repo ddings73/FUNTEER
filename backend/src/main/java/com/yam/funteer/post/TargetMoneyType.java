c77f7a34fa23d3d8eb6ca6ec33d11113552fe38f
ddings <ddings7303@gmail.com>
2023-01-26 17:24:40 +0900

    🔧 : 기타
    Former-commit-id: a6be9e0296d041cc922f8374939dd517d5d31ba6

--- backend/src/main/java/com/yam/funteer/post/TargetMoneyType.java
+++ backend/src/main/java/com/yam/funteer/post/TargetMoneyType.java
@@ -7,6 +7,8 @@ import lombok.ToString;
 @Getter @ToString
 @RequiredArgsConstructor
 public enum TargetMoneyType{
-	LEVEL_ONE("1단계"), LEVEL_TWO("2단계"), LEVEL_THREE("3단계");
+	LEVEL_ONE("1단계"),
+	LEVEL_TWO("2단계"),
+	LEVEL_THREE("3단계");
 	private final String description;
 }
