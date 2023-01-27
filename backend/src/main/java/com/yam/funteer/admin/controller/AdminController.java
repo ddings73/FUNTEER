99d75d9ee0d4ca244054f14545a25bbe52d741c4
bbookng <bbookng@gmail.com>
2023-01-27 10:16:31 +0900

    🐛 AdminController 수정
    Former-commit-id: 4b3eef3617a347a591aadf4ac12d5a8b184bf1b2

--- backend/src/main/java/com/yam/funteer/admin/controller/AdminController.java
+++ backend/src/main/java/com/yam/funteer/admin/controller/AdminController.java
@@ -6,13 +6,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yam.funteer.admin.dto.MemberListResponse;
 import com.yam.funteer.admin.dto.TeamListResponse;
+import com.yam.funteer.funding.dto.FundingDetailResponse;
 import com.yam.funteer.funding.dto.FundingListResponse;
+import com.yam.funteer.funding.dto.FundingRequest;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -48,15 +52,21 @@ public class AdminController {
 		return null;
 	}
 
+	@ApiOperation(value = "모든 펀딩 목록 조회", notes = "모든 펀딩 목록을 조회한다.")
+	@GetMapping("/funding")
+	public ResponseEntity<List<FundingListResponse>> findAllFunding() {
+		return null;
+	}
+
 	@ApiOperation(value = "승인 대기 펀딩 목록 조회", notes = "승인 대기중인 펀딩 목록을 조회한다.")
 	@GetMapping("/funding/wait")
 	public ResponseEntity<List<FundingListResponse>> findWaitFundingList() {
 		return null;
 	}
 
-	@ApiOperation(value = "모든 펀딩 목록 조회", notes = "모든 펀딩 목록을 조회한다.")
-	@GetMapping("/funding")
-	public ResponseEntity<List<FundingListResponse>> findAllFunding() {
+	@ApiOperation(value = "펀딩 상세 조회", notes = "펀딩 상세를 조회한다.")
+	@GetMapping("/funding/{fundingId}")
+	public ResponseEntity<FundingDetailResponse> readFundingDetail(@PathVariable Long fundingId) {
 		return null;
 	}
 
