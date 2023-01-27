38d0ffdd7a85e1b584338202073d24fb81e8f46d
김승섭 <sub9707@naver.com>
2023-01-27 09:38:36 +0900

    🔧 RV6 수정
    Former-commit-id: 2921fe09c103c81b97555b0b631fe15e1f27f236

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,39 +6,98 @@ import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store/store';
-import UserRoot from './UserRoot'
-import { Login, MainPage } from './pages';
-import AdminRoot from './AdminRoot';
-
+/*  */
+import UserRoot from './roots/UserRoot';
+import AdminRoot from './roots/AdminRoot';
+import {
+  MainPage,
+  SignUp,
+  MemberSignUp,
+  TeamSignUp,
+  FindPassword,
+  Login,
+  TeamPage,
+  ServiceDetail,
+  FindEmail,
+  ResetPassword,
+  Charge,
+  Donation,
+  ErrorPage,
+} from './pages/index';
 
 const router = createBrowserRouter([
   {
-   path:"/",
-   element:<UserRoot />,
-   children:[
-    {
-      index:true,
-      element:<MainPage/>
-    },
-    {
-      path:'/login',
-      element:<Login/>
-    }
-   ] 
+    path: '/',
+    element: <UserRoot />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <MainPage />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'findEmail',
+        element: <FindEmail />,
+      },
+      {
+        path: 'findPassword',
+        element: <FindPassword />,
+        children: [
+          {
+            path: 'ResetPassword',
+            element: <ResetPassword />,
+          },
+        ],
+      },
+      {
+        path: 'signup',
+        element: <SignUp />,
+        children: [
+          {
+            path: 'teamSignUp',
+            element: <TeamSignUp />,
+          },
+          {
+            path: 'MemberSignUp',
+            element: <MemberSignUp />,
+          },
+        ],
+      },
+      {
+        path: 'donation',
+        element: <Donation />,
+      },
+      {
+        path: 'service',
+        element: <ServiceDetail />,
+      },
+      {
+        path: 'team',
+        element: <TeamPage />,
+      },
+      {
+        path: 'charge',
+        element: <Charge />,
+      },
+    ],
   },
   {
-    path:"/admin",
-    element:<AdminRoot/>,
-    children:[
-    ]
-  }
-])
+    path: '/admin',
+    element: <AdminRoot />,
+    errorElement: <ErrorPage />,
+    children: [],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 );
