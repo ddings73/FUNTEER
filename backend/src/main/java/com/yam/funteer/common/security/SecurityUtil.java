f1d5d7f6dfa36b41c151403d69e2c3454f4e62b3
ddings <ddings7303@gmail.com>
2023-01-29 19:06:19 +0900

    ✨ : SecurityUtil 추가
    Former-commit-id: 78ca224a2d44f90f42e47f9095a83576d9ddcd06

--- backend/src/main/java/com/yam/funteer/common/security/SecurityUtil.java
+++ backend/src/main/java/com/yam/funteer/common/security/SecurityUtil.java
@@ -6,15 +6,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.util.Optional;
+
 @Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SecurityUtil {
-    public static Long getCurrentUserId(){
+    public static Optional<Long> getCurrentUserId(){
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || authentication.getName() == null) {
-            throw new RuntimeException("Security Context 에 인증 정보가 없습니다.");
+            log.error("Security Context 에 인증 정보가 없습니다.");
+            return Optional.empty();
         }
 
-        return Long.valueOf(authentication.getName());
+        Long userId = Long.valueOf(authentication.getName());
+        return Optional.of(userId);
     }
 }
