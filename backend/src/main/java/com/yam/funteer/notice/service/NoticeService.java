5f8e1ff45b155405c73a158c05214415ad8048b9
rlthdqls <rlthdqls@gmail.com>
2023-01-30 17:14:19 +0900

    ✨ notice
    Former-commit-id: 83c51258b1a5ba50bbb719437cf73ca1deab1de8

--- backend/src/main/java/com/yam/funteer/notice/service/NoticeService.java
+++ backend/src/main/java/com/yam/funteer/notice/service/NoticeService.java
@@ -1,4 +1,17 @@
 package com.yam.funteer.notice.service;
 
+import java.io.IOException;
+import java.util.List;
+
+import com.yam.funteer.faq.dto.response.FaqListRes;
+import com.yam.funteer.notice.dto.request.NoticeRegistReq;
+import com.yam.funteer.notice.dto.response.NoticeBaseRes;
+
 public interface NoticeService {
+	List<FaqListRes>noticeGetList();
+	NoticeBaseRes noticeGetDetail(Long postId);
+	NoticeBaseRes noticeRegister(NoticeRegistReq noticeRegistReq) throws IOException;
+	NoticeBaseRes noticeModify(Long postId,NoticeRegistReq noticeRegistReq) throws IOException;
+	void noticeDelete(Long postId);
+
 }
