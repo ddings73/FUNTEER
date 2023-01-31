d4fad6de61ba591059f520829912c72543d42816
백준봉 <bjb2992@naver.com>
2023-01-31 17:42:26 +0900

    🔨 FAQ 화면 구성
    Former-commit-id: b66bcfc180e01b2eafadd8d4dbaf7893183c9c66

--- frontend/src/containers/CustomerCenter/FAQ/FAQContainerMemberItem.tsx
+++ frontend/src/containers/CustomerCenter/FAQ/FAQContainerMemberItem.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-function FAQContainerMemberItem() {
-  return <div>FAQContainerMemberItem</div>;
-}
+type FAQItemType = {
+  title: string;
+  content: string;
+};
+
+const FAQContainerMemberItem: FAQItemType[] = [
+  {
+    title: '김진호는 왜 키보드가 필요한가요?',
+    content: '몰라여 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ',
+  },
+];
 
 export default FAQContainerMemberItem;
