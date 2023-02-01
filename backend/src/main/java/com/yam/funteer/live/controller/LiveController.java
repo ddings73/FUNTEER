123817a4ecce4f967e5701d2e4cc34f725e5bc1e
ddings <ddings7303@gmail.com>
2023-02-01 23:40:46 +0900

    ✨ : OPENVIDU 테스트
    Former-commit-id: ab0ffd258d61d5e73a8f69de893f8fdce1fcea2c

--- backend/src/main/java/com/yam/funteer/live/controller/LiveController.java
+++ backend/src/main/java/com/yam/funteer/live/controller/LiveController.java
@@ -1,4 +1,47 @@
 package com.yam.funteer.live.controller;
 
+import io.openvidu.java.client.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/openvidu")
 public class LiveController {
+
+    @Value("${OPENVIDU.URL}")
+    private String OPENVIDU_URL;
+
+    @Value("${OPENVIDU.SECRET}")
+    private String OPENVIDU_SECRET;
+
+    private OpenVidu openVidu;
+
+    @PostConstruct
+    public void init(){
+        this.openVidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
+    }
+
+    @PostMapping("/sessions")
+    public ResponseEntity<String> initializeSession(@RequestBody(required = false) Map<String, Object> params) throws OpenViduJavaClientException, OpenViduHttpException {
+        SessionProperties properties = SessionProperties.fromJson(params).build();
+        Session session = openVidu.createSession(properties);
+        return ResponseEntity.ok(session.getSessionId());
+    }
+
+    @PostMapping("/sessions/{sessionId}/connections")
+    public ResponseEntity<String> createConnection(@PathVariable String sessionId,
+                                                   @RequestBody(required = false) Map<String, Object> params) throws OpenViduJavaClientException, OpenViduHttpException {
+        Session session = openVidu.getActiveSession(sessionId);
+        if(session == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
+        Connection connection = session.createConnection(properties);
+        return ResponseEntity.ok(connection.getToken());
+    }
 }
