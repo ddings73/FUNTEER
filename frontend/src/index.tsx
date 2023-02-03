fa372aba7b9837efeae2d62a68093edb997b5629
백준봉 <bjb2992@naver.com>
2023-02-03 09:02:49 +0900

    🔨 react-persist 주석 처리
    Former-commit-id: 8b32522bbc7ebc02daa2eee04de4b48eb496a4f2

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,8 +4,8 @@ import './index.css';
 import { BrowserRouter, createBrowserRouter, RouterProvider, useParams } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from '@emotion/react';
-import { persistStore } from 'redux-persist';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { persistStore } from 'redux-persist';
+// import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store/store';
@@ -207,13 +207,13 @@ const router = createBrowserRouter([
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-const persistor = persistStore(store);
+// const persistor = persistStore(store);
 
 root.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <RouterProvider router={router} />
-    </PersistGate>
+    {/* <PersistGate loading={null} persistor={persistor}> */}
+    <RouterProvider router={router} />
+    {/* </PersistGate> */}
   </Provider>,
 );
 
--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,16 +1,16 @@
 import { configureStore, combineReducers, getDefaultMiddleware } from '@reduxjs/toolkit';
-import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
 import userSlice from './slices/userSlice';
 import modalSlice from './slices/modalSlice';
 import fileModalSlice from './slices/fileModalSlice';
 import payModalSlice from './slices/payModalSlice';
 
-const persistConfig = {
-  key: 'root',
-  version: 1,
-  storage,
-};
+// const persistConfig = {
+//   key: 'root',
+//   version: 1,
+//   storage,
+// };
 
 const rootReducer = combineReducers({
   userSlice,
@@ -19,14 +19,15 @@ const rootReducer = combineReducers({
   payModalSlice,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: persistedReducer,
+  reducer: rootReducer,
   middleware: getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
+    serializableCheck: false,
+    // {
+    //   ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    // }
   }),
 });
 
@@ -35,6 +36,6 @@ export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
 
 export default store;
