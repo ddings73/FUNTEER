33d4a86b4cff455b80803d88e0ab407767d16ad2
ddings <ddings7303@gmail.com>
2023-02-05 17:56:16 +0900

    🔨 : 코드 리팩토링
    Former-commit-id: 48896753e2fad747b32577f24117abd75dd66e7e

--- backend/src/main/java/com/yam/funteer/badge/service/BadgeServiceImpl.java
+++ backend/src/main/java/com/yam/funteer/badge/service/BadgeServiceImpl.java
@@ -35,7 +35,7 @@ public class BadgeServiceImpl implements BadgeService {
 	@Override
 	public void initBadges(User user) {
 		if(user.getUserType().equals(UserType.TEAM_WAIT)){
-			List<Badge> badgeList=badgeRepository.findAllByIdBetween(Long.valueOf(9),Long.valueOf(17));
+			List<Badge> badgeList=badgeRepository.findAllByIdBetween(9L, 17L);
 			for(Badge badge:badgeList){
 				UserBadge userBadge=UserBadge.builder()
 					.user(user)
@@ -44,7 +44,7 @@ public class BadgeServiceImpl implements BadgeService {
 				userBadgeRepository.save(userBadge);
 			}
 		}else if(!user.getUserType().equals(UserType.ADMIN)){
-			List<Badge> badgeList=badgeRepository.findAllByIdBetween(Long.valueOf(1),Long.valueOf(14));
+			List<Badge> badgeList=badgeRepository.findAllByIdBetween(1L, 14L);
 			for(Badge badge:badgeList){
 				UserBadge userBadge=UserBadge.builder()
 					.user(user)
@@ -71,24 +71,24 @@ public class BadgeServiceImpl implements BadgeService {
 		Integer count=paymentList.size();
 		if ( count>=5){
 			if(postGroup.equals(PostGroup.DONATION)){
-				badgeIds.add(Long.valueOf(12));
+				badgeIds.add(12L);
 			}else if(postGroup.equals(PostGroup.FUNDING)){
-				badgeIds.add(Long.valueOf(5));
+				badgeIds.add(5L);
 			}
 		}
 		if( count>=10) {
 			if(postGroup.equals(PostGroup.DONATION)){
-				badgeIds.add(Long.valueOf(13));
+				badgeIds.add(13L);
 			}else if(postGroup.equals(PostGroup.FUNDING)){
-				badgeIds.add(Long.valueOf(6));
+				badgeIds.add(6L);
 			}
 
 		}
 		if(count>=30){
 			if(postGroup.equals(PostGroup.DONATION)){
-				badgeIds.add(Long.valueOf(14));
+				badgeIds.add(14L);
 			}else if(postGroup.equals(PostGroup.FUNDING)){
-				badgeIds.add(Long.valueOf(7));
+				badgeIds.add(7L);
 			}
 		}
 	}
@@ -97,29 +97,29 @@ public class BadgeServiceImpl implements BadgeService {
 		Long amount=paymentList.stream().mapToLong(payment -> payment.getAmount()).sum();
 		if ( amount>=50000){
 			if(postGroup.equals(PostGroup.DONATION)){
-				badgeIds.add(Long.valueOf(9));
+				badgeIds.add(9L);
 			}else if(postGroup.equals(PostGroup.FUNDING)){
-				badgeIds.add(Long.valueOf(2));
+				badgeIds.add(2L);
 			}
 		}
 		if( amount>=100000) {
 			if(postGroup.equals(PostGroup.DONATION)){
-				badgeIds.add(Long.valueOf(10));
+				badgeIds.add(10L);
 			}else if(postGroup.equals(PostGroup.FUNDING)){
-				badgeIds.add(Long.valueOf(3));
+				badgeIds.add(3L);
 			}
 
 		}
 		if(amount>=300000){
 			if(postGroup.equals(PostGroup.DONATION)){
-				badgeIds.add(Long.valueOf(11));
+				badgeIds.add(11L);
 			}else if(postGroup.equals(PostGroup.FUNDING)){
-				badgeIds.add(Long.valueOf(4));
+				badgeIds.add(4L);
 			}
 		}
 	}
 
-	public void cal(User user,List<Long>ids){
+	private void cal(User user,List<Long>ids){
 		for(Long id:ids) {
 			UserBadge userBadge =
 				userBadgeRepository.findByUserAndBadge(user, badgeRepository.findById(id)
@@ -132,7 +132,7 @@ public class BadgeServiceImpl implements BadgeService {
 	@Override
 	public void totalPayAmount(User user) {
 		Long amount=paymentRepository.findAllByUser(user).stream().mapToLong(payment -> payment.getAmount()).sum();
-		UserBadge userBadge=userBadgeRepository.findByUserAndBadge(user,badgeRepository.findById(Long.valueOf(1))
+		UserBadge userBadge=userBadgeRepository.findByUserAndBadge(user,badgeRepository.findById(1L)
 			.orElseThrow(IllegalArgumentException::new));
 		if(amount>=1000000){
 			userBadge.achieve();
@@ -145,14 +145,13 @@ public class BadgeServiceImpl implements BadgeService {
 		Integer count=fundingList.size();
 		List<Long>ids=new ArrayList<>();
 		if(count>=5){
-			ids.add(Long.valueOf(15));
+			ids.add(15L);
 		}
 		if(count>=10){
-			ids.add(Long.valueOf(16));
-
+			ids.add(16L);
 		}
 		if(count>=50){
-			ids.add(Long.valueOf(17));
+			ids.add(17L);
 		}
 		cal(team,ids);
 	}
