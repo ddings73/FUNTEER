6eaeb4152e456550698bf608f7fb7bb061616935
bangbongbim <37106496+bangbongbim@users.noreply.github.com>
2023-02-08 22:25:12 +0900

    🐛 경로수정
    Former-commit-id: a5b7a48a0e85c3e7d47230092a5a4188231ae4f0

--- frontend/src/pages/Live/PublisherLiveRoom.tsx
+++ frontend/src/pages/Live/PublisherLiveRoom.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PublisherLiveRoomContainer from '../../containers/Live/PublisherLiveRoomContainer';
 
+// 팀으로 열기
 function PublisherLiveRoom() {
   return <PublisherLiveRoomContainer />;
 }
