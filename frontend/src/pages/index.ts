157338281b8f16821c9514e8d09bb65586f5c2dc
bangbongbim <37106496+bangbongbim@users.noreply.github.com>
2023-02-08 22:11:00 +0900

    🐛  라이브 경로 수정
    Former-commit-id: e6a684b4b3d873c8efa709f4055743bac4c50737

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -51,6 +51,8 @@ export { default as TeamProfile } from './TeamPage/TeamProfile';
 export { default as TeamEdit } from './TeamPage/TeamEdit';
 export { default as TeamDonation } from './TeamPage/TeamDonation';
 
+
+// 라이브
 export { default as CreateLive } from './Live/CreateLive';
 export { default as PublisherLiveRoom } from './Live/PublisherLiveRoom';
 export { default as SubscribeLiveRoom } from './Live/SubscribeLiveRoom';
