75ac6408cb8da7c2cf10317731f0342683ccee5f
becoding96 <becoding96@gmail.com>
2023-02-10 00:12:28 +0900

    🔨 도네이션 관리 임시 저장
    Former-commit-id: 93878554c496a425805f5ac2afe2217a29e47d27

--- frontend/src/containers/Admin/AdminDonation/AdminDonationContainer.tsx
+++ frontend/src/containers/Admin/AdminDonation/AdminDonationContainer.tsx
@@ -1,72 +1,121 @@
 import React, { useEffect, useState } from 'react';
-import { useInView } from 'react-intersection-observer';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import MenuItem from '@mui/material/MenuItem';
+import Pagination from '@mui/material/Pagination';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-import AdminDonationContainerItem, { DonationState } from './AdminDonationContainerItem';
-import { requestAdminDonationList, requestDonationList, requestDonationStatus, requestNextAdminDonationList } from '../../../api/donation';
-import { DonationListElementType, DonationStatusModi } from '../../../types/donation';
-import styles from './AdminDonationListContainer.module.scss';
+import { requestAdminDonationList, requestDonationStatus } from '../../../api/donation';
+import { DonationListElementType } from '../../../types/donation';
+import styles from './AdminDonationContainer.module.scss';
+
+enum DonationState {
+  All = '전체',
+  DONATION_ACTIVE = '진행중',
+  DONATION_CLOSE = '종료',
+}
+
+const donationStateMap = new Map<string, string>([
+  ['DONATION_ACTIVE', '진행중'],
+  ['DONATION_CLOSE', '종료'],
+]);
 
 function AdminDonationContainer() {
-  const size = 10;
-  const {pathname}=useLocation();
+  const navigate = useNavigate();
+  const [page, setPage] = useState<number>(1);
+  const [maxPage, setMaxPage] = useState<number>(0);
   const [donationList, setDonationList] = useState<DonationListElementType[]>([]);
-  const [ref, inView] = useInView();
-  const [donationStatusModi, setDonationStatusModi] = useState<DonationStatusModi>(
-    {
-      postType:"",
-      donationId:0
-    }
-  );
-  const navigate=useNavigate();
+  const [donationFilter, setDonationFilter] = useState<string>(DonationState.All);
 
-  const onStateChangeHandler = async (id: number, state: string, e: SelectChangeEvent) => {
+  useEffect(() => {
+    if (!maxPage) {
+      getMaxPage();
+    } else {
+      requestPageDonations();
+    }
+  }, [page]);
+
+  const onStateChangeHandler = async (id: number, state: string) => {
     if (state === 'DONATION_ACTIVE') {
-      
-      // // long id requesetBody =>json 
-      // setDonationStatusModi({...donationStatusModi,postType:state})
-      // setDonationStatusModi({...donationStatusModi,donationId:id})
-      try{
+      try {
         const response = await requestDonationStatus(id, state);
         console.log(response);
-      }catch(error){
+      } catch (error) {
         console.log(error);
       }
-    } 
+    }
     window.location.reload();
   };
-  const onClickDonationItemHandler = (id:number) => {
-    console.log('도네이션 관리 상세 페이지 이동');
-    // {pathvaliable}
-    navigate(`${id}`)
+
+  const onClickDonationItemHandler = (id: number) => {
+    navigate(`${id}`);
   };
 
-  const onClickDonationRegister=()=>{
-    console.log("작성페이지로");
-    // admin/donation/creat
+  const onClickDonationRegister = () => {
     navigate('create');
-  }
+  };
 
-  const requestGetDonationList = async () => {
-    try {
-      const response = await requestAdminDonationList(size);
-      console.log(response)
-      setDonationList(response.data)
-    } catch (error) {
-      console.error(error)
+  const handleChangeFilter = (e: SelectChangeEvent<string>) => {
+    setDonationFilter(e.target.value);
+  };
+
+  const filtedDonations = donationList.filter((donation) => {
+    let filter;
+
+    if (donationFilter === '전체') {
+      filter = true;
+    } else {
+      filter = donationStateMap.get(donation.postType) === donationFilter;
     }
-  }
 
-  useEffect(()=>{
-    requestGetDonationList();
-  },[])
+    return filter;
+  });
+
+  /** 페이지 교체 */
+  const handleChangePage = (e: React.ChangeEvent<any>, selectedPage: number) => {
+    setPage(selectedPage);
+  };
+
+  /** 최대 페이지 구하기 */
+  const getMaxPage = async () => {
+    try {
+      const response = await requestAdminDonationList(10000);
+      const pageCalc = response.data.length % 8 ? response.data.length / 8 + 1 : response.data.length / 8;
+      setMaxPage(pageCalc);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  /** 페이지 요청 */
+  const requestPageDonations = async () => {
+    setDonationList([]);
+    try {
+      const response = await requestAdminDonationList(8, page - 1);
+      console.log(response);
+      setDonationList(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const donationStateSet = Object.values(DonationState);
 
   return (
     <div className={styles.container}>
       <div className={styles.contents}>
         <h1 className={styles.title}>도네이션 관리</h1>
-        <button type="button" onClick={onClickDonationRegister} style={{margin:'0rem 0rem 1rem auto'}}>도네이션 작성</button>
+
+        <div className={styles.filter}>
+          <button type="button" onClick={onClickDonationRegister} className={styles.create}>
+            도네이션 작성
+          </button>
+          <Select value={donationFilter} onChange={handleChangeFilter} sx={{ height: '25px', margin: '0 0 0 0.5rem', fontSize: '0.9rem', fontFamily: 'NanumSquare' }}>
+            {donationStateSet.map((state) => (
+              <MenuItem key={state} value={state} sx={{ height: '25px', fontSize: '0.9rem', fontFamily: 'NanumSquare' }}>
+                {state}
+              </MenuItem>
+            ))}
+          </Select>
+        </div>
 
         <ul className={styles['title-line']}>
           <li>번호</li>
@@ -76,13 +125,13 @@ function AdminDonationContainer() {
           <li>종료일</li>
           <li>상태</li>
         </ul>
-        
-        {donationList.map((data) => (
+
+        {filtedDonations.map((data) => (
           <div className={styles['list-line']}>
             <li>
               <p>{data.id}</p>
             </li>
-            <button type="button" className={styles['title-col-btn']} onClick={(e)=>onClickDonationItemHandler(data.id)}>
+            <button type="button" className={styles['title-col-btn']} onClick={() => onClickDonationItemHandler(data.id)}>
               <li>
                 <p>{data.title}</p>
               </li>
@@ -97,18 +146,25 @@ function AdminDonationContainer() {
               <p>{data.endDate}</p>
             </li>
             <li>
-            <Select
+              <Select
+                color="warning"
                 value={data.postType}
-                onChange={(e) => onStateChangeHandler(data.id, data.postType, e)}
+                onChange={() => onStateChangeHandler(data.id, data.postType)}
                 className={data.postType.includes('ACTIVE') ? styles['show-approve'] : styles['hide-approve']}
               >
-                <MenuItem value='DONATION_ACTIVE'>진행중</MenuItem>
-                <MenuItem value='DONATION_CLOSE'>종료</MenuItem>
+                <MenuItem value="DONATION_ACTIVE" sx={{ fontSize: '0.9rem', fontFamily: 'NanumSquare' }}>
+                  진행중
+                </MenuItem>
+                <MenuItem value="DONATION_CLOSE" sx={{ fontSize: '0.9rem', fontFamily: 'NanumSquare' }}>
+                  종료
+                </MenuItem>
               </Select>
               <p className={data.postType.includes('ACTIVE') ? styles['hide-approve'] : styles.quit}>종료</p>
             </li>
           </div>
         ))}
+
+        <Pagination sx={{ marginTop: '2rem' }} count={maxPage} page={page} onChange={handleChangePage} />
       </div>
     </div>
   );
