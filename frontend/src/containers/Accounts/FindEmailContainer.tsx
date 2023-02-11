8ac8e078bd42dd1c9fbb9bc7594aa420380b08e0
bangbongbim <37106496+bangbongbim@users.noreply.github.com>
2023-02-11 19:26:11 +0900

    ✨ 아이디, 비밀번호 찾기 완성
    Former-commit-id: 809e9e8062209c91603af00ed1750afd8d60f68d

--- frontend/src/containers/Accounts/FindEmailContainer.tsx
+++ frontend/src/containers/Accounts/FindEmailContainer.tsx
@@ -1,10 +1,11 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from '@mui/material';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { requestFindEmail } from '../../api/user';
 import styles from './FindEmailContainer.module.scss';
 
 type UserLoginType = {
-  email: string;
+  name: string;
   phone: string;
 };
 
@@ -12,19 +13,35 @@ function FindEmailContainer() {
   const navigate = useNavigate();
   const [onModal, setOnModal] = useState(false);
   const [userInfo, setUserInfo] = useState<UserLoginType>({
-    email: '',
+    name: '',
     phone: '',
   });
+  const [email,setEmail] = useState<string>("")
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
 
-    setUserInfo({ ...userInfo, [name]: value });
+    if(name ==='phone'){
+     const newPhone = value.replace(/[^0-9]/g, '') // 숫자를 제외한 모든 문자 제거
+      .replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`);
+
+      setUserInfo({...userInfo,phone:newPhone})
+    }
+    else 
+       setUserInfo({...userInfo,name:value})
+
   };
 
   const findEmail = async () => {
-    console.log(userInfo);
+    try{
+      const response = await requestFindEmail(userInfo.name,userInfo.phone)
+      console.log(response)
 
+      setEmail(response.data.email)
+    }
+    catch(error){
+      console.log(error)
+    }
     setOnModal(!onModal);
   };
 
@@ -36,14 +53,15 @@ function FindEmailContainer() {
       <div className={styles.contents}>
         <div className={styles['login-box']}>
           <p className={styles.title}>이메일 찾기</p>
-          <p>이메일</p>
-          <TextField onChange={onChangeHandler} name="email" margin="normal" id="outlined-basic" placeholder="가입시 등록한 이메일을 입력해주세요." variant="outlined" />
+          <p>이름</p>
+          <TextField onChange={onChangeHandler} value={userInfo.name} name="name" margin="normal" id="outlined-basic" placeholder="가입시 입력한 회원님의 이름을 입력해주세요." variant="outlined" />
           <p>전화번호</p>
           <TextField
             onChange={onChangeHandler}
             name="phone"
             type="text"
             margin="normal"
+            value={userInfo.phone}
             id="outlined-basic"
             placeholder="가입시 등록한 전화번호를 입력해주세요."
             variant="outlined"
@@ -55,7 +73,7 @@ function FindEmailContainer() {
           <Dialog open={onModal} onClose={handleClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
             <DialogTitle id="alert-dialog-title">이메일 찾기</DialogTitle>
             <DialogContent>
-              <DialogContentText id="alert-dialog-description">회원님의 이메일은 ddd@ddd.com입니다</DialogContentText>
+              <DialogContentText id="alert-dialog-description">회원님의 이메일은 {email}입니다</DialogContentText>
             </DialogContent>
             <DialogActions>
               <Button onClick={handleClose}>닫기</Button>
