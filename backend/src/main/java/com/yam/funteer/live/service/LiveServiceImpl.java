4b076c41ae24c491745aae2bdafdd12bbbea186d
ddings <ddings7303@gmail.com>
2023-02-13 00:20:59 +0900

    🔨 : 유저 잔고확인 코드 추가
    Former-commit-id: 20895e40c094f933d8a2053360268f2b7f8c01ed

--- backend/src/main/java/com/yam/funteer/live/service/LiveServiceImpl.java
+++ backend/src/main/java/com/yam/funteer/live/service/LiveServiceImpl.java
@@ -286,6 +286,8 @@ public class LiveServiceImpl implements LiveService{
             Member member = memberRepository.findById(userId).orElseThrow(UserNotFoundException::new);
             Long amount = request.getAmount();
 
+            member.checkMoney(amount);
+
             Team team = live.getTeam();
             team.updateMoney(amount);
             member.updateMoney(-amount);
