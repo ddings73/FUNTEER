7a7698d7202cd17b6de1cd03b67879e48b420c26
bangbongbim <dkdpdl12311@gmail.com>
2023-02-14 02:01:18 +0900

    ✨ 뱃지 기본 디자인 제작
    Former-commit-id: 28fc36dc755987aae7f5f54b8021f1a0a6e63b75

--- frontend/src/containers/MyPage/MyBadgesContainer.tsx
+++ frontend/src/containers/MyPage/MyBadgesContainer.tsx
@@ -1,20 +1,21 @@
 /* eslint-disable react/no-array-index-key */
 import React, { useEffect, useState } from 'react';
+import cn from 'classnames';
+import { Tooltip } from '@mui/material';
 import styles from './MyBadgesContainer.module.scss';
 import SideBarList from '../../components/MyPageSideBar/SideBarList';
 import { requestUserProfile } from '../../api/user';
 import { useAppSelector } from '../../store/hooks';
 
-
-type badgeType ={
-  achieve:boolean,
-  badgeImgPath:string,
-  description:string,
-  name:string
-}
+type badgeType = {
+  achieve: boolean;
+  badgeImgPath: string;
+  description: string;
+  name: string;
+};
 
 export function MyPageContainer() {
-  const [badges,setBadges] = useState<badgeType[]>([])
+  const [badges, setBadges] = useState<badgeType[]>([]);
   const userId = useAppSelector((state) => state.userSlice.userId);
 
   const getRequestUserInfo = async () => {
@@ -32,20 +33,27 @@ export function MyPageContainer() {
     getRequestUserInfo();
   }, []);
 
-  useEffect(()=>{
-    console.log(badges)
-  },[badges])
+  useEffect(() => {
+    console.log(badges);
+  }, [badges]);
   return (
     <div className={styles.bodyContainer}>
       <SideBarList />
       <div className={styles.contentContainer}>
         <div className={styles.contentBox}>
-
           <div className={styles.badgeBox}>
-            
-            {badges.map((badge,index)=>(
-              <img src={badge.badgeImgPath} alt="뱃지" key={index+1}/>
-            ))}
+            <p className={styles.badgeTitle}>획득한 뱃지 현황</p>
+
+            <div className={styles.badgeContentsBox}>
+              {badges.map((badge, index) => (
+                <Tooltip title={badge.description} placement="top">
+                  <div className={styles.badgeStyle}>
+                    <img src={badge.badgeImgPath} alt="뱃지" key={index + 1} className={cn(badge.achieve ? styles.achieve : '')} />
+                    <p>{badge.name}</p>
+                  </div>
+                </Tooltip>
+              ))}
+            </div>
           </div>
         </div>
       </div>
