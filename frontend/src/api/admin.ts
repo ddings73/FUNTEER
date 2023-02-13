900158daa3c2ad51617089641710a1c374087de8
bbookng <bbookng@gmail.com>
2023-02-13 13:43:14 +0900

    ✨ Admin Funding 수정중
    - 내용1
    - 내용2
    
    
    Former-commit-id: 739fdb21a2f104605bfe2d8182204d5bd0edf707

--- frontend/src/api/admin.ts
+++ frontend/src/api/admin.ts
@@ -81,7 +81,7 @@ export const requestAdminSearchedFundingList = async (page: number, size: number
     size,
   };
 
-  const response = http.get('funding/search', { params });
+  const response = http.get('admin/funding/search', { params });
 
   return response;
 };
