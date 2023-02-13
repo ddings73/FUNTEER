e615d4329ce21b770699b714774c583ca367c27f
bangbongbim <37106496+bangbongbim@users.noreply.github.com>
2023-02-14 01:30:10 +0900

    🔧 뱃지
    Former-commit-id: 464e39ef8cbab406f10860e652cde7fba9ecf2fd

--- frontend/src/containers/MyPage/MyBadgesContainer.tsx
+++ frontend/src/containers/MyPage/MyBadgesContainer.tsx
@@ -1,13 +1,53 @@
-import React from 'react';
-import styles from './MyPageContainer.module.scss';
+/* eslint-disable react/no-array-index-key */
+import React, { useEffect, useState } from 'react';
+import styles from './MyBadgesContainer.module.scss';
 import SideBarList from '../../components/MyPageSideBar/SideBarList';
+import { requestUserProfile } from '../../api/user';
+import { useAppSelector } from '../../store/hooks';
+
+
+type badgeType ={
+  achieve:boolean,
+  badgeImgPath:string,
+  description:string,
+  name:string
+}
 
 export function MyPageContainer() {
+  const [badges,setBadges] = useState<badgeType[]>([])
+  const userId = useAppSelector((state) => state.userSlice.userId);
+
+  const getRequestUserInfo = async () => {
+    try {
+      const response = await requestUserProfile(userId);
+      console.log(response.data);
+
+      setBadges([...response.data.myBadges]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getRequestUserInfo();
+  }, []);
+
+  useEffect(()=>{
+    console.log(badges)
+  },[badges])
   return (
     <div className={styles.bodyContainer}>
       <SideBarList />
       <div className={styles.contentContainer}>
-        <div className={styles.contentBox}>중앙</div>
+        <div className={styles.contentBox}>
+
+          <div className={styles.badgeBox}>
+            
+            {badges.map((badge,index)=>(
+              <img src={badge.badgeImgPath} alt="뱃지" key={index+1}/>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
