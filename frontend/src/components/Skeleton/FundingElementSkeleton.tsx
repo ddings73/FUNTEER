072f4887948c791d9d3cd523961b165332e3c690
bangbongbim <37106496+bangbongbim@users.noreply.github.com>
2023-02-16 01:30:04 +0900

    ✨ 펀딩리스트  대공사
    Former-commit-id: 8397e9dc14256aa0574e48188b276cef93de15a5

--- frontend/src/components/Skeleton/FundingElementSkeleton.tsx
+++ frontend/src/components/Skeleton/FundingElementSkeleton.tsx
@@ -6,8 +6,88 @@ import React from 'react'
 
 function FundingElementSkeleton(){
     return (
-        <Box sx={{display:'flex', flexDirection:'row',justifyContent:"space-between"}}>
+        <Box sx={{display:'flex', flexDirection:'row',justifyContent:"space-between",flexWrap:"wrap"}}>
        <Box sx={{marginTop:"25px",boxSizing:"border-box",padding:"15px"}}>
+           <Skeleton variant="rectangular" width={250} height={230} animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '2.5rem' }} width={200} animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '0.8rem' }} width={250} animation={false} />
+              <Box sx={{display:'flex',flexDirection:"row",justifyContent:"space-between",width:"250px"}} >
+              <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={80}animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={80}animation="wave" />
+              </Box>
+            <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={250}  animation="wave"/>
+        </Box>
+        <Box sx={{marginTop:"25px",boxSizing:"border-box",padding:"15px"}}>
+           <Skeleton variant="rectangular" width={250} height={230} animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '2.5rem' }} width={200} animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '0.8rem' }} width={250} animation={false} />
+              <Box sx={{display:'flex',flexDirection:"row",justifyContent:"space-between",width:"250px"}} >
+              <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={80}animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={80}animation="wave" />
+              </Box>
+            <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={250}  animation="wave"/>
+        </Box>
+        <Box sx={{marginTop:"25px",boxSizing:"border-box",padding:"15px"}}>
+           <Skeleton variant="rectangular" width={250} height={230} animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '2.5rem' }} width={200} animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '0.8rem' }} width={250} animation={false} />
+              <Box sx={{display:'flex',flexDirection:"row",justifyContent:"space-between",width:"250px"}} >
+              <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={80}animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={80}animation="wave" />
+              </Box>
+            <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={250}  animation="wave"/>
+        </Box>
+        <Box sx={{marginTop:"25px",boxSizing:"border-box",padding:"15px"}}>
+           <Skeleton variant="rectangular" width={250} height={230} animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '2.5rem' }} width={200} animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '0.8rem' }} width={250} animation={false} />
+              <Box sx={{display:'flex',flexDirection:"row",justifyContent:"space-between",width:"250px"}} >
+              <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={80}animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={80}animation="wave" />
+              </Box>
+            <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={250}  animation="wave"/>
+        </Box>
+        <Box sx={{marginTop:"25px",boxSizing:"border-box",padding:"15px"}}>
+           <Skeleton variant="rectangular" width={250} height={230} animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '2.5rem' }} width={200} animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '0.8rem' }} width={250} animation={false} />
+              <Box sx={{display:'flex',flexDirection:"row",justifyContent:"space-between",width:"250px"}} >
+              <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={80}animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={80}animation="wave" />
+              </Box>
+            <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={250}  animation="wave"/>
+        </Box>
+        <Box sx={{marginTop:"25px",boxSizing:"border-box",padding:"15px"}}>
+           <Skeleton variant="rectangular" width={250} height={230} animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '2.5rem' }} width={200} animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '0.8rem' }} width={250} animation={false} />
+              <Box sx={{display:'flex',flexDirection:"row",justifyContent:"space-between",width:"250px"}} >
+              <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={80}animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={80}animation="wave" />
+              </Box>
+            <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={250}  animation="wave"/>
+        </Box>
+        <Box sx={{marginTop:"25px",boxSizing:"border-box",padding:"15px"}}>
+           <Skeleton variant="rectangular" width={250} height={230} animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '2.5rem' }} width={200} animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '0.8rem' }} width={250} animation={false} />
+              <Box sx={{display:'flex',flexDirection:"row",justifyContent:"space-between",width:"250px"}} >
+              <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={80}animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={80}animation="wave" />
+              </Box>
+            <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={250}  animation="wave"/>
+        </Box>
+        <Box sx={{marginTop:"25px",boxSizing:"border-box",padding:"15px"}}>
+           <Skeleton variant="rectangular" width={250} height={230} animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '2.5rem' }} width={200} animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '0.8rem' }} width={250} animation={false} />
+              <Box sx={{display:'flex',flexDirection:"row",justifyContent:"space-between",width:"250px"}} >
+              <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={80}animation="wave" />
+              <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={80}animation="wave" />
+              </Box>
+            <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={250}  animation="wave"/>
+        </Box>
+        <Box sx={{marginTop:"25px",boxSizing:"border-box",padding:"15px"}}>
            <Skeleton variant="rectangular" width={250} height={230} animation="wave" />
               <Skeleton variant="text" sx={{ fontSize: '2.5rem' }} width={200} animation="wave" />
               <Skeleton variant="text" sx={{ fontSize: '0.8rem' }} width={250} animation={false} />
